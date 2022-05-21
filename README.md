aa6626f7eefcb8f8a78de3d12aa8d43f17c37ff8
milena-kos <75163549+milena-kos@users.noreply.github.com>
2022-05-21 20:44:33 +0300

    push all the stuff

---
+++ README.md
@@ -0,0 +1,41 @@
+# The Abyss For The Puzzle Cell
+Abyss is a proxy Hack for The Puzzle Cell, its like middle-man between you and server so it can spoof your packets without modifying neither client or server code.
+
+# How To
+## Run
+Grab latest stable from releases and run it, enter IP of destination server and connect to `localhost:6969` in your TPC.
+
+## Build
+- Install dependencies using `pip install -r requirements.txt`
+- Install pyinstaller `pip install pyinstaller`
+- cd to abyss-tpc-main
+- `pyinstaller --onefile --icon=NONE main.py`
+
+# Da Hacks List
+
+## Nerf Anti-Cheat
+Prevents you from sending some forbidden packets
+
+## Invisibility
+Hides your cursor
+
+## Multiplayern't
+Hides everyone's cursors
+
+## Cleaner
+Will replace all incoming cells with void (aka forcing erase mode for everyone)
+
+## Force Sandbox Mode
+Switches you to sandbox mode while level mode is active
+
+## Printer
+Prints all sent and recieved packets
+
+## Show Yourself
+Allows you to see your own cursor
+
+## Hover Flex
+Shows your last placed cell as a hover on your cursor
+
+# More hacks
+Feel free to suggest more!
---
+++ main.py
@@ -0,0 +1,164 @@
+import asyncio, websockets, threading, colorama, time
+
+async def hello(localclient):
+    global client_id, exiting
+    async with websockets.connect(REMOTE_URL) as remote:
+        client_id = None
+        print("Connected! Enter digits of hacks to toggle:")
+        
+        taskA = asyncio.create_task(clientToServer(remote, localclient))
+        taskB = asyncio.create_task(serverToClient(remote, localclient))
+        input_thread = threading.Thread(target=toggler, args=(hacks_enabled,))
+        input_thread.start()
+
+        await taskA
+        await taskB
+
+def toggler(hacks_enabled):
+    global deinit_yourself, deinit_yourself_2, exiting
+    deinit_yourself = False
+    deinit_yourself_2 = False
+    colorama.init(autoreset=True)
+    while True:
+        prefixes = {}
+        for i in range(1, 9):
+            try:
+                if hacks_enabled[i]:
+                    prefixes[i] = colorama.Fore.GREEN + str(i) + ". "
+                else:
+                    prefixes[i] = colorama.Fore.RED + str(i) + ". "
+            except:
+                prefixes[i] = colorama.Fore.RED + str(i) + ". "
+        print("\n\n\n\n\n\n")
+        print(prefixes[1] + "Nerf Anti-Cheat - prevents you from sending some forbidden packets") # done
+        print(prefixes[2] + "Invisibility - hides your cursor") # done
+        print(prefixes[3] + "Multiplayern't - hides everyone's cursors") # done
+        print(prefixes[4] + "Cleaner - will replace all incoming cells with void (aka forcing erase mode for everyone)") # done
+        print(prefixes[5] + "Force Sandbox Mode - switches you to sandbox mode while level mode is active") # done
+        print(prefixes[6] + "Printer - prints all sent and recieved packets") # done
+        print(prefixes[7] + "Show Yourself - allows you to see your own cursor") # done
+        print(prefixes[8] + "Hover Flex - shows your last placed cell as a hover on your cursor") # done
+        print('"exit" to exit')
+        
+        try:
+            hack = input("Toggle: ")
+            hack = int(hack)
+            try:
+                hacks_enabled[hack] = not hacks_enabled[hack]
+                if hacks_enabled[hack] == False and hack == 7:
+                    deinit_yourself = True
+                if hacks_enabled[hack] == False and hack == 8:
+                    deinit_yourself_2 = True
+            except:
+                hacks_enabled[hack] = True
+        except:
+            if hack == "exit":
+                exiting = True
+                print("Abyss will close on next packet.")
+                break
+            else:
+                print("Invalid hack!")
+
+def is_hack(value):
+    global hacks_enabled
+    try:
+        return hacks_enabled[value]
+    except:
+        hacks_enabled[value] = False
+        return hacks_enabled[value]
+
+async def serverToClient(remote, localclient):
+    global hacks_enabled, client_id, cellid, exiting
+    async for message in localclient:
+        if exiting:
+            await remote.close()
+            await localclient.close()
+            break
+        do_send = True
+        if message.startswith("set-cursor") and client_id == None:
+            stuff = message.split(" ")
+            client_id = stuff[1]
+        if (is_hack(2) or is_hack(3)) and message.startswith("set-cursor") and message.split(" ")[1] == client_id:
+            message = "set-cursor " + client_id + " 69420.87006875000003 69420.13745000000001"
+        if is_hack(4) and message.startswith("place"):
+            x = message.split(" ")[1]
+            y = message.split(" ")[2]
+            message = f"place {x} {y} empty 0 0"
+        if is_hack(8) and message.startswith("place"):
+            x = message.split(" ")[1]
+            y = message.split(" ")[2]
+            cellid = message.split(" ")[3]
+            new_message = f"new-hover {client_id} {x} {y} {cellid} 0"
+            await remote.send(new_message)
+            
+            new_message = f"set-cursor ItWorkedButYouCantSeeIt {x} {y}"
+            await localclient.send(new_message)
+        if is_hack(1) and (message.startswith("edtype") or message.startswith("remove-cursor")):
+            do_send = False
+        if do_send:
+            if is_hack(6):
+                print("> " + message)
+            await remote.send(message)
+    raise KeyboardInterrupt
+
+async def clientToServer(remote, localclient):
+    global client_id, deinit_yourself, deinit_yourself_2
+    async for message in remote:
+        if deinit_yourself and message.startswith("set-cursor") and message.split(" ")[1] == client_id:
+            deinit_yourself = False
+            name = message.split(" ")[1] + "_"
+            new_message = f"set-cursor {name} 69420.87006875000003 69420.13745000000001"
+            await localclient.send(new_message)
+        if deinit_yourself_2 and message.startswith("set-cursor") and message.split(" ")[1] == client_id:
+            deinit_yourself_2 = False
+            name = message.split(" ")[1] + "_"
+            new_message = "set-cursor ItWorkedButYouCantSeeIt 69420.87006875000003 69420.13745000000001"
+            await localclient.send(new_message)
+            new_message = "drop-hover " + client_id
+            await remote.send(new_message)
+        if is_hack(3) and message.startswith("set-cursor"):
+            new_message = "set-cursor " + message.split(" ")[1] + " 69420.87006875000003 69420.13745000000001"
+            if is_hack(6):
+                print("> " + message)
+            await remote.send(new_message)
+        if is_hack(5):
+            await localclient.send("edtype sandbox")
+            hacks_enabled[5] = False
+        if is_hack(4) and message.startswith("place"):
+            x = message.split(" ")[1]
+            y = message.split(" ")[2]
+            new_message = f"place {x} {y} empty 0 0"
+            if is_hack(6):
+                print("> " + new_message)
+            await remote.send(new_message)
+        if is_hack(7) and message.startswith("set-cursor") and message.split(" ")[1] == client_id:
+            name = message.split(" ")[1] + "_"
+            x = message.split(" ")[2]
+            y = message.split(" ")[3]
+            new_message = f"set-cursor {name} {x} {y}"
+            await localclient.send(new_message)
+            
+        if is_hack(6):
+            print("< " + message)
+        await localclient.send(message)
+
+async def main():
+    global exiting
+    async with websockets.serve(hello, "localhost", 6969):
+        print("Started proxy! Connect to 127.0.0.1:6969 in your TPC to start...")
+        if not exiting:
+            await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    exiting = False
+    print("Welcome to Abyss for TPC by Milenakos#3310!")
+    REMOTE_URL = input("Enter server you want to connect to: ")
+    hacks_enabled = {}
+    if "repl.co" in REMOTE_URL and not REMOTE_URL.startswith("ws"):
+        REMOTE_URL = "wss://" + REMOTE_URL
+    elif not REMOTE_URL.startswith("ws"):
+        REMOTE_URL = "ws://" + REMOTE_URL
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Goodbye!")
